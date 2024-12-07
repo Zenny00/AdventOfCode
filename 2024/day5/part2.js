b823d520f5978c60f18e8e581417c2feb4294efa
Zenny00 <joshuacomfort@gmail.com>
2024-12-07 00:08:29 -0500

    Day 5

--- 2024/day5/part2.js
+++ 2024/day5/part2.js
@@ -1,5 +1,41 @@
 const fs = require('node:fs');
 
+function fix_row(row, rules) {
+  let valid = true;
+  let i = 0;
+  let seen = [];
+  while (valid && i < row.length) {
+    let number = row[i];
+    let not_before = order_map[number];
+
+    // If we've already seen any of the numbers we're not supposed to this row is invalid
+    
+    if (not_before) {
+      let swap_index = 0;
+      let invalid = seen.some((e, i) => {
+          let result = not_before.includes(e);
+          swap_index = i;
+          return result;
+      });
+
+      if (invalid) {
+        let tmp = row[swap_index];
+        row[swap_index] = row[i];
+        row[i] = tmp;
+
+        i = 0;
+        seen = [];
+        continue;
+      }
+    }
+
+    seen.push(number);
+    i++;
+  }
+
+  return row;
+}
+
 function readData(filename) {
   try {
     const data = fs.readFileSync('./' + filename, 'utf8');
@@ -10,7 +46,7 @@ function readData(filename) {
   }
 }
 
-const file_data = readData('input2');
+const file_data = readData('input');
 const data = file_data.split("\n");
 
 const order_rules = [];
@@ -42,20 +78,25 @@ for (let i = 0; i < order_rules.length; i++) {
   }
 }
 
+console.log(order_map)
+
+let broken_rows = []; 
 let valid_rows = [];
+loop1:
 for (let i = 0; i < rows.length; i++) {
   let valid_row = true;
   let row = rows[i];
   let numbers = row.split(",");
   let seen = [];
+loop2:
   for (let j = 0; j < numbers.length; j++) {
     let number = numbers[j];
     let not_before = order_map[number];
 
     // If we've already seen any of the numbers we're not supposed to this row is invalid
     if (not_before && seen.some((e) => not_before.includes(e))) {
-      valid_row = false;
-      console.log(numbers);
+      broken_rows.push(fix_row(numbers, order_map));
+      break loop2;
     }
 
     seen.push(number);
@@ -63,13 +104,14 @@ for (let i = 0; i < rows.length; i++) {
 
   if (valid_row) {
     valid_rows.push(numbers);
-    
   }
 }
 
+console.log(broken_rows);
+
 let middle_sum = 0;
-for (let i = 0; i < valid_rows.length; i++) {
-  let row = valid_rows[i];
+for (let i = 0; i < broken_rows.length; i++) {
+  let row = broken_rows[i];
   let number = Number(row[Math.floor(row.length/2)]);
   middle_sum += number;
 }
